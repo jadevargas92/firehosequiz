afe19cfd3a1ee345a6443735c38f3ce8ffb03c79
Jade Vargas <jadevargas92@gmail.com>
2016-04-15 04:51:04 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firehosequiz::Application.config.secret_key_base = 'd7a5b50d309dfef13364316fcb14f467ae9a025d05d1da5851cc0df758e5ba08fd5d9d44131e3e619b5f841650cfacdd7d753d89afade95403e03b3a4c778c78'
